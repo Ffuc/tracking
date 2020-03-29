8f02616c29a73f04e52887153975c9292dcf5bde
dream jun <lwj1472586>
2020-03-29 11:05:07 +0800

    流水铺号 service 修改

--- tracking/src/main/java/com/ruixun/tracking/controller/TrackingWaterDetailsController.java
+++ tracking/src/main/java/com/ruixun/tracking/controller/TrackingWaterDetailsController.java
@@ -9,31 +9,29 @@ import com.ruixun.tracking.service.ITrackingWaterService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @Api("铺流水账目详情")
 @RequestMapping("/water")
 public class TrackingWaterDetailsController {
-
     @Autowired
     private ITrackingWaterService iTrackingWaterService;
 
-    @RequestMapping("/findWaterDetails")
+    @PostMapping("/findWaterDetails")
     @ApiOperation("铺流水账目 需传递参数 1.时间2.桌号3.靴号  startTime  tableId  bootId")
-    public Result findWaterDetails(TrackingAgencyAccountsDto trackingAgencyAccountsDto, @RequestParam(defaultValue = "1") Integer current){
+    public Result findWaterDetails(@RequestBody TrackingAgencyAccountsDto trackingAgencyAccountsDto,@RequestBody @RequestParam(defaultValue = "1") Integer current){
         IPage<Map<String, Object>> mapIPage
                 = iTrackingWaterService.waterAccounts(trackingAgencyAccountsDto, current);
         return ResultResponseUtil.ok().msg("查询成功").data(mapIPage);
     }
 
-    @RequestMapping("/findWaterDetails")
+    @PostMapping("/waterDetails")
     @ApiOperation("铺流水账目-修改结果详情 参数 1.时间2.桌号3.靴号  startTime  tableId  bootId")
-    public Result fin(TrackingAgencyAccountsDto trackingAgencyAccountsDto, @RequestParam(defaultValue = "1") Integer current){
+    public Result fin(@RequestBody TrackingAgencyAccountsDto trackingAgencyAccountsDto, @RequestBody @RequestParam(defaultValue = "1") Integer current){
         IPage<Map<String, Object>> mapIPage
                 = iTrackingWaterService.waterDetails(trackingAgencyAccountsDto, current);
         return ResultResponseUtil.ok().msg("查询成功").data(mapIPage);
